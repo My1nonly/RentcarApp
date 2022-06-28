439dbec53b0f74010550ebd1f4ad09f4553e19b5
My1nonly <hafizdiangga@gmail.com>
2022-06-28 09:04:29 +0700

    commit_hafiz

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,33 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+Route::get('index', function () {
+    return view('index');
+});
+
+Route::get('about-us', function () {
+    return view('about-us');
+});
+
+Route::get('contact', function () {
+    return view('contact');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('upload-car',[CarDetailController::class,'create']);
+Route::get('car-listing',[CarDetailController::class,'index']);
+Route::get('cardetail/{car_details}',[CarDetailController::class,'show']);
+Route::get('myposts/{car_details}',[CarDetailController::class,'userpost']);
+Route::get('editcar/{CarDetail}',[CarDetailController::class,'edit']);
+Route::get('deletecar/{car_details}',[CarDetailController::class,'destroy']);
+Route::get('updatecar/{car_details}',[CarDetailController::class,'update']);
+Route::get('withdriver',[CarDetailController::class,'withdriver']);
+Route::get('withoutdriver',[CarDetailController::class,'withoutdriver']);
+Route::get('allcars',[CarDetailController::class,'allcars']);
+
+Route::get('reg-users',[RegisterController::class,'usersdata']);
